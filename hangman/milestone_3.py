6e9f0013ff1ab77372553693ee85d46b238f9253
Arhan-K <arhankamath@gmail.com>
2023-11-12 23:41:50 +0000

    Second commit

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,4 +1,4 @@
-#import milestone_2
+import milestone_2
 while True:
     # Custom input validation without explicitly raising ValueError
     guess = input("Please enter a single letter: ")
@@ -7,7 +7,7 @@ while True:
     else:
         raise ValueError("Invalid letter. Please, enter a single alphabetical character.")
 
-#if guess in milestone_2.word():
-#    print(f"Good guess! {guess} is in the word.")
-#else:
-#    print(f"Sorry, {guess} is not in the word. Try again.")
+if guess in milestone_2.word():
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
