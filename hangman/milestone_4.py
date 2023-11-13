956db3129c9e91528da88d5639b4f0a5cb99a21f
Arhan-K <arhankamath@gmail.com>
2023-11-13 11:49:43 +0000

    M4 C8

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -11,6 +11,7 @@ class Hangman:
         self.num_list = 0 # number of unguessed unique letters
         self.list_of_guesses = [] #number of wrong guesses
         self.word = random.choice(word_list)
+        self.num_letters = len(self.word)
         self.word_guessed = []
         for self.letter in self.word:
             self.word_guessed.append("_")
@@ -26,8 +27,17 @@ class Hangman:
         guess = guess.lower()
         if guess in milestone_2.word:
             print(f"Good guess! {guess} is in the word.")
+            #indexes = []
+            for i in range(len(self.word)):
+                if self.word[i] == guess:
+                    self.word_guessed[i] = (guess)
+                    #indexes.append(i)
+            self.num_letters -= 1
+            
         else:
-            print(f"Sorry, {guess} is not in the word. Try again.")    
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word. Try again.")
+            print(f"You have {self.num_lives} left") 
         #return self.param1 + ext_input + Cylinder.att
 
     def ask_for_input(self):  # method to modify attribute
@@ -40,5 +50,6 @@ class Hangman:
             else:              
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess)
+                print(self.list_of_guesses)
 hangman = Hangman()
 hangman.ask_for_input()
