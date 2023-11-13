60d881cb6c5acdcec9c1cb8fcc09bc6a0513e188
Arhan-K <arhankamath@gmail.com>
2023-11-13 11:11:15 +0000

    M2 C4

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,10 +1,10 @@
 import random
 word_list = ["mango", "raspberry", "pineapple", "blackberry", "lychee"]
-word = random.choice(word_list)
-guess = input("Please enter a single letter: ")
-if len(guess) == 1 and guess.isalpha():
-    print("Good guess")
-else:
-    print( "Oops! That is not a valid input.")
-print(word_list)
-print(word)
+#word = random.choice(word_list)
+#guess = input("Please enter a single letter: ")
+#if len(guess) == 1 and guess.isalpha():
+#    print("Good guess")
+#else:
+#    print( "Oops! That is not a valid input.")
+#print(word_list)
+#print(word)
